0294e0259414fd2c35c88c02b27ec418e722281d
Tim Hoogstrate <tim566@hotmail.com>
2023-07-20 10:56:58 +0200

    squash commit

---
+++ packages/video_player/video_player/video_player_avfoundation/pigeons/messages.dart
@@ -0,0 +1,106 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/src/messages.g.dart',
+  dartTestOut: 'test/test_api.g.dart',
+  objcHeaderOut: 'ios/Classes/messages.g.h',
+  objcSourceOut: 'ios/Classes/messages.g.m',
+  objcOptions: ObjcOptions(
+    prefix: 'FLT',
+  ),
+  copyrightHeader: 'pigeons/copyright.txt',
+))
+class TextureMessage {
+  TextureMessage(this.textureId);
+  int textureId;
+}
+
+class LoopingMessage {
+  LoopingMessage(this.textureId, this.isLooping);
+  int textureId;
+  bool isLooping;
+}
+
+class IsSupportedMessage {
+  IsSupportedMessage(this.isSupported);
+  bool isSupported;
+}
+
+class IsCacheSupportedMessage {
+  IsCacheSupportedMessage(this.url);
+  String url;
+}
+
+class VolumeMessage {
+  VolumeMessage(this.textureId, this.volume);
+  int textureId;
+  double volume;
+}
+
+class ClearCacheMessage {
+  ClearCacheMessage(this.textureId);
+  int textureId;
+}
+
+class PlaybackSpeedMessage {
+  PlaybackSpeedMessage(this.textureId, this.speed);
+  int textureId;
+  double speed;
+}
+
+class PositionMessage {
+  PositionMessage(this.textureId, this.position);
+  int textureId;
+  int position;
+}
+
+class CreateMessage {
+  CreateMessage({required this.httpHeaders, required this.enableCache});
+  String? asset;
+  String? uri;
+  String? packageName;
+  String? formatHint;
+  bool enableCache;
+  Map<String?, String?> httpHeaders;
+}
+
+class MixWithOthersMessage {
+  MixWithOthersMessage(this.mixWithOthers);
+  bool mixWithOthers;
+}
+
+@HostApi(dartHostTestHandler: 'TestHostVideoPlayerApi')
+abstract class AVFoundationVideoPlayerApi {
+  @ObjCSelector('initialize')
+  void initialize();
+  @ObjCSelector('create:')
+  TextureMessage create(CreateMessage msg);
+  @ObjCSelector('dispose:')
+  void dispose(TextureMessage msg);
+  @ObjCSelector('setLooping:')
+  void setLooping(LoopingMessage msg);
+  @ObjCSelector('clearCache:')
+  void clearCache(ClearCacheMessage msg);
+  @ObjCSelector('setVolume:')
+  void setVolume(VolumeMessage msg);
+  @ObjCSelector('isCacheSupportedForNetworkMedia:')
+  IsSupportedMessage isCacheSupportedForNetworkMedia(
+      IsCacheSupportedMessage msg);
+  @ObjCSelector('setPlaybackSpeed:')
+  void setPlaybackSpeed(PlaybackSpeedMessage msg);
+  @ObjCSelector('play:')
+  void play(TextureMessage msg);
+  @ObjCSelector('position:')
+  PositionMessage position(TextureMessage msg);
+  @async
+  @ObjCSelector('seekTo:')
+  void seekTo(PositionMessage msg);
+  @ObjCSelector('pause:')
+  void pause(TextureMessage msg);
+  @ObjCSelector('setMixWithOthers:')
+  void setMixWithOthers(MixWithOthersMessage msg);
+}
